43081d4dcaffab562d78b28a6e14a8caa4e0d14d
Mehmet-Unluturk <unluturkmehmet0@gmail.com>
2023-09-11 18:15:29 +0300

    Update main file

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
+
 from bs4 import BeautifulSoup
 import requests
 
 base_url = 'https://dergipark.org.tr'
-page_number = 1  # İlk sayfa
-max_pages = 5  # Kaç sayfa taranacak
+page_number = 1  
+max_pages = 5  
 
-while page_number <= max_pages:
-    
-    url = f'https://dergipark.org.tr/tr/search/{page_number}?section=journal'
-    html_text = requests.get(url).text
-    soup = BeautifulSoup(html_text, 'lxml')
-
-    
-    dergiler = soup.find_all('div', class_='card kt-mb-20 journal-card dp-card-outline')
-
-    
-    for dergi in dergiler:
-        isim = dergi.find('h5', class_='card-title').text
-        badge_element = dergi.find('div', class_='year-badge')
-        sayi_element = dergi.find('div', class_='col-6 text-left')
-        indir_element = dergi.find('div', class_='col-6 text-right')
+with open('dergipark_files.txt', 'w', encoding='utf-8') as file:
+    while page_number <= max_pages:
+        
+        url = f'https://dergipark.org.tr/tr/search/{page_number}?section=journal'
+        html_text = requests.get(url).text
+        soup = BeautifulSoup(html_text, 'lxml')
 
         
-        badge = badge_element.text.strip() if badge_element else "Veri Yok"
-        sayi = sayi_element.text.replace(' ', '') if sayi_element else "Veri Yok"
-        indir = indir_element.text if indir_element else "Veri Yok"
+        magazines = soup.find_all('div', class_='card kt-mb-20 journal-card dp-card-outline')
 
-        print(isim)
-        print(badge)
-        print(sayi)
-        print(indir)
-        print("--------------------------")
+        
+        for magazine in magazines:
+            magazine_name = magazine.find('h5', class_='card-title').text
+            badge_element = magazine.find('div', class_='year-badge')
+            issues_per_year_element = magazine.find('div', class_='col-6 text-left')
+            views_and_downloads_element = magazine.find('div', class_='col-6 text-right')
 
-    page_number += 1
+            
+            badge = badge_element.text.strip() if badge_element else "Veri Yok"
+            issues_per_year = issues_per_year_element.text.replace(' ', '') if issues_per_year_element else "Veri Yok"
+            views_and_downloads = views_and_downloads_element.text if views_and_downloads_element else "Veri Yok"
 
+            print(magazine_name)
+            print(badge)
+            print(issues_per_year)
+            print(views_and_downloads)
+            print("--------------------------")
+
+            file.write(f'Magazine_Name: {magazine_name}\n\n')
+            file.write(f'Badge: {badge}\n\n')
+            file.write(f'İssues_Per_Year: {issues_per_year}\n\n')
+            file.write(f'Views_And_Downloads: {views_and_downloads}\n\n')
+            file.write("--------------------------\n\n")
+
+        page_number += 1
+
+print("Data saved to file: dergipark_files.txt")
 
 
 
